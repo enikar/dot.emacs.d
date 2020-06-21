853d1868ce2e3d9daae26ca4a286800e5f02fce5
enikar <enikar@gresille.org>
2020-06-21 20:59:52 +0200

    Put some settings in early-init.el to speed up the start up.

--- early-init.el
+++ early-init.el
@@ -5,4 +5,25 @@
       ("gnu" . "https://elpa.gnu.org/packages/")
       ("org" . "https://orgmode.org/elpa/")))
 
-;; (setq package-quickstart t)
+(setq gc-cons-threshold 10000000)
+
+;; (setq initial-frame-alist
+;;       '((top . 1) (left . 1) (width . 80) (height . 35)))
+;;         ;(foreground-color . "black")
+;;         ;(background-color . "snow2"))
+
+
+(setq default-frame-alist
+      '((top . 0)
+        (left . 0)
+        (width . 158)
+        (height . 40)
+        (menu-bar-lines 0)
+        (tool-bar-lines 0)
+        (vertical-scroll-bars)
+        (font . "-PfEd-Inconsolata-normal-normal-normal-*-24-*-*-*-m-0-iso10646-1")
+        ))
+
+(menu-bar-mode 0)
+
+(advice-add #'x-apply-session-resources :override #'ignore)
--- init.el
+++ init.el
@@ -1,27 +1,7 @@
 (setq inhibit-startup-message t)
 (setq inhibit-startup-echo-area-message "karine")
-(setq gc-cons-threshold 10000000)
-
-;; (setq initial-frame-alist
-;;       '((top . 1) (left . 1) (width . 80) (height . 35)))
-;;         ;(foreground-color . "black")
-;;         ;(background-color . "snow2"))
-
-
-(setq default-frame-alist
-      '((top . 0)
-        (left . 0)
-        (width . 158)
-        (height . 40)
-        (menu-bar-lines 0)
-        (tool-bar-lines 0)
-        (vertical-scroll-bars)
-        (font . "-PfEd-Inconsolata-normal-normal-normal-*-24-*-*-*-m-0-iso10646-1")
-        ))
 
 (setq-default indicate-empty-lines t)
-
-(menu-bar-mode 0)
 
 
 (add-to-list 'custom-theme-load-path (file-name-as-directory "~/.emacs.d/themes/") )
@@ -30,7 +10,7 @@
 
 (load "~/.emacs.d/load-packages")
 (load "~/.emacs.d/programming-modes")
-(load  "~/.emacs.d/bindings")
+(load "~/.emacs.d/bindings")
 
 (recentf-mode)
 (setq recentf-exclude
@@ -51,7 +31,6 @@
 (defalias 'man-mode 'Man-mode)
 (setq comint-scroll-show-maximum-output t)
 (setq comint-scroll-to-bottom-on-input t)
-;(setq line-number-mode t)
 
 (setq scroll-step 1)
 (setq-default tab-width 4)
@@ -64,6 +43,3 @@
 (put 'upcase-region 'disabled nil)
 (put 'scroll-left 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
-
-;; try to load evil-eyebrowse in load-package
-;; (load "~/.emacs.d/evil-eyebrowse")
