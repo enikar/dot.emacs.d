02f0b893317212575f0f9ebb07453caa15d798b5
enikar <enikar@chezlefab.net>
2021-03-10 17:48:54 +0100

    Remove org repositry for package-archives.

--- early-init.el
+++ early-init.el
@@ -1,8 +1,8 @@
 (setq package-user-dir "~/.emacs.d/elpa")
 (setq package-archives
     '(("melpa" . "https://melpa.org/packages/")
-      ("gnu"   . "https://elpa.gnu.org/packages/")
-      ("org"   . "https://orgmode.org/elpa/")))
+      ("gnu"   . "https://elpa.gnu.org/packages/")))
+      ;; ("org"   . "https://orgmode.org/elpa/")))
 
 (setq load-prefer-newer t)
 
