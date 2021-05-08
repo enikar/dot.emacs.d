c38abac5980b4e2f14804321487b40470d56e1f9
enikar <enikar@chezlefab.net>
2021-05-08 16:26:58 +0200

    Changed options of persistent-scratch

--- elisp/perso/epilogue.el
+++ elisp/perso/epilogue.el
@@ -6,13 +6,15 @@
 ;; (eval-when-compile
 ;;   (require 'use-package))
 
+;; provoque l'erreur :
+;; Eager macro-expansion failure: (wrong-type-argument listp [(first-item . rest-items) (sp-get-list-items)])
 (use-package persistent-scratch
+  :init (progn
+          (persistent-scratch-autosave-mode t)
+          (ignore-errors (persistent-scratch-restore)))
+
   :custom (persistent-scratch-save-file "~/.emacs.d/.cache/persistent-scratch")
-          (persistent-scratch-what-to-save '(major-mode point narrowing text-properties))
-  :init
-  (progn
-    (persistent-scratch-autosave-mode t)
-    (ignore-errors (persistent-scratch-restore))))
+          (persistent-scratch-what-to-save '(major-mode point narrowing text-properties)))
 
 (use-package eyebrowse
   :custom (eyebrowse-mode-line-style 'always)
