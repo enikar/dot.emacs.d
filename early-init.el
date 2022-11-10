65a72e522775ceced0294d7844390097584b38ef
enikar <enikar@chezlefab.net>
2022-11-10 10:41:59 +0100

    Add a hook to show the emacs startuo time.

--- early-init.el
+++ early-init.el
@@ -1,3 +1,18 @@
+;; -*- lexical-binding: t -*-
+;; Increases Garbage Collection During Startup
+;;(defvar startup/gc-cons-threshold gc-cons-threshold)
+(setq gc-cons-threshold most-positive-fixnum)
+(add-hook 'emacs-startup-hook
+          #'(lambda()
+              (setq gc-cons-threshold (expt 2 23))))
+
+(add-hook 'emacs-startup-hook
+          #'(lambda ()
+              (message "*** Emacs loaded in %.2f with %d garbage collections."
+                       (float-time
+                        (time-subtract after-init-time before-init-time))
+                       gcs-done)))
+
 (setq package-user-dir "~/.emacs.d/elpa")
 (setq package-archives
     '(("melpa" . "https://melpa.org/packages/")
@@ -15,18 +30,6 @@
 
 (setq load-prefer-newer t)
 
-;; Increases Garbage Collection During Startup
-(defvar startup/gc-cons-threshold gc-cons-threshold)
-(setq gc-cons-threshold most-positive-fixnum)
-(add-hook 'emacs-startup-hook
-          #'(lambda()
-              (setq gc-cons-threshold startup/gc-cons-threshold)))
-
-;; (setq initial-frame-alist
-;;       '((top . 1) (left . 1) (width . 80) (height . 35)))
-;;         ;(foreground-color . "black")
-;;         ;(background-color . "snow2"))
-
 
 (advice-add #'x-apply-session-resources :override #'ignore)
 (setq default-frame-alist
@@ -43,3 +46,20 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+
+;;;; Dissociate (TAB, C-i), (ESC, C-[) and (RET, C-m).
+
+;; We win 3 key chords when unsing a graphical display.
+;; We can bind them with `(define-key [C-i] #'command-to-be-bound)' or
+;; `(define-key (kbd "<C-i>") #'command-to-be-bound)' for example.
+;; Notice [C-i] and (Kbd "<C-i>") identify the same key code while
+;; (kbd "C-i") returns a literal TAB character.
+
+;; (add-hook
+;;  'after-make-frame-functions
+;;  (defun setup-blah-keys (frame)
+;;    (with-selected-frame frame
+;;      (when (display-graphic-p)
+;;        (define-key input-decode-map (kbd "C-i") [C-i])
+;;        (define-key input-decode-map (kbd "C-[") [C-lsb])
+;;        (define-key input-decode-map (kbd "C-m") [C-m])))))
