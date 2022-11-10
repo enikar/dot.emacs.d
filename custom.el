3f71a5325e2989a76030e799c5001d724565ccb9
enikar <enikar@chezlefab.net>
2022-11-10 10:40:42 +0100

    Replace the obsolete use of `window-system'.

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(Info-fontify (eq window-system 'x))
+ '(Info-fontify (display-graphic-p))
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
