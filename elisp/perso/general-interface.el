a22cc995513649e7832afcfc3fd6bd6a47f4d1da
enikar <enikar@gresille.org>
2026-09-06 10:52:32 +0200

    Remove remember package configuration.

--- elisp/perso/general-interface.el
+++ elisp/perso/general-interface.el
@@ -1465,9 +1465,6 @@ argument, query for word to search."
 (use-package eshell-vterm
   :hook (eshell-mode . eshell-vterm-mode))
 
-(use-package remember
-  :init (leader-ala-vim "C-r" #'remember)
-  (setq remember-data-file (my/put-this-in-var "notes.txt")))
 
 (repeat-mode 1)
 (setq repeat-exit-timeout 5
