0d0f4e3c81376d16808193acd86f9e96f0142a77
enikar <enikar@gresille.org>
2026-09-10 10:29:36 +0200

    Remove haskell-interactive-mode from my/mode-in-emacs-state.
    As well remove also haskell-error-mode which I never use.
    Add an advice to the function haskell-interactive-mode to be
    in emacs-state.

--- elisp/perso/programming.el
+++ elisp/perso/programming.el
@@ -112,8 +112,11 @@ If the error list is visible, hide it.  Otherwise, show it."
          (ghci-script-mode . my/no-auto-fill))
   :diminish (interactive-haskell-mode)
   :init
-    (push 'haskell-error-mode my/mode-in-emacs-state)
-    (push 'haskell-interactive-mode my/mode-in-emacs-state)
+    ;; I'm not sure if I ever use haskell-error-mode.
+    ;; Anyway I use dante + flycheck
+    ;;(push 'haskell-error-mode my/mode-in-emacs-state)
+    ;;(push 'haskell-interactive-mode my/mode-in-emacs-state)
+    (advice-add #'haskell-interactive-mode :after #'evil-emacs-state)
     (setq haskell-process-args-ghci '("+RTS" "-M5G" "-RTS" "-ferror-spans")
           haskell-process-log t
           haskell-process-suggest-hoogle-imports t
