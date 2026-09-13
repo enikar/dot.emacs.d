4d29768e838f7990b75b10d89255a6b99cae4b8f
enikar <enikar@gresille.org>
2026-09-13 22:02:29 +0200

    Remove the old commented out orderless configuration.

--- elisp/perso/general-interface.el
+++ elisp/perso/general-interface.el
@@ -896,12 +896,6 @@ To use it: (push 'a-mode my/mode-in-emacs-state)")
   :general (:keymaps 'minibuffer-local-map
                      "M-m"  #'marginalia-cycle))
 
-;; (use-package orderless
-;;   :init (setq completion-styles '(substring orderless basic)
-;;               completion-category-defaults nil
-;;               completion-category-overrides '((files (style partial-completion)))))
-
-
 (use-package consult-dir
   :defer t
   :init (general-def "C-x C-d"  #'consult-dir)
